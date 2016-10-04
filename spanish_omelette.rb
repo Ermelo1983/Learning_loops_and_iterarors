f47d9a4990c97cd45584bdf1a136e3bf0416b57b
Ermelo1983 <ermelo1983@gmail.com>
2016-10-04 16:21:46 +0200

    Define steps by making methods

--- spanish_omelette.rb
+++ spanish_omelette.rb
@@ -46,25 +46,55 @@ loop do
   end
 
   steps = [
-    { description: "Scrape potatoes", action: "scrape_potatoes"},
-    { description: "Cut potatoes into thick slices", action: "cut_patatoes"},
-    { description: "Chop the onion", action: "generic_recipe_step"},
-    { description: "Heat the oil in a large frying pan", action: "generic_recipe_step"},
-    { description: "Add potatoes to the pan", action: "add_patatoes_to_the_pan"},
-    { description: "Add onion to the pan", action: "generic_recipe_step" },
-    { description: "Strain potatoes and onions through a colander into a large bowl", action: "generic_recipe_step" },
-    { description: "Break the eggs", action: "break_eggs" },
-    { description: "Beat the eggs separately", action: "generic_recipe_step" },
-    { description: "Stir into the bowl the potatoes with plenty of salt", action: "generic_recipe_step" },
-    { description: "Heat a little of the strained oil in a smaller pan", action: "generic_recipe_step" },
-    { description: "Tip everything into the pan and cook on a moderate heat", action: "generic_recipe_step"},
-    { description: "When almost set, invert on a plate and slide back into the pan", action: "generic_recipe_step" },
-    { description: "Cook a few more minutes and slide on to a plate and cool for 10 minutes before serving", action: "generic_recipe_step" }
+    { description: "Scrape potatoes.", action: "scrape_potatoes"},
+    { description: "Cut potatoes into thick slices.", action: "cut_potatoes"},
+    { description: "Chop the onion.", action: "generic_recipe_step"},
+    { description: "Heat the oil in a large frying pan.", action: "generic_recipe_step"},
+    { description: "Add potatoes to the pan.", action: "add_potatoes_to_the_pan"},
+    { description: "Add onion to the pan.", action: "generic_recipe_step" },
+    { description: "Strain potatoes and onions through a colander into a large bowl.", action: "generic_recipe_step" },
+    { description: "Break the eggs.", action: "break_eggs" },
+    { description: "Beat the eggs separately.", action: "generic_recipe_step" },
+    { description: "Stir into the bowl the potatoes with plenty of salt.", action: "generic_recipe_step" },
+    { description: "Heat a little of the strained oil in a smaller pan.", action: "generic_recipe_step" },
+    { description: "Tip everything into the pan and cook on a moderate heat.", action: "generic_recipe_step"},
+    { description: "When almost set, invert on a plate and slide back into the pan.", action: "generic_recipe_step" },
+    { description: "Cook a few more minutes and slide on to a plate and cool for 10 minutes before serving.", action: "generic_recipe_step" }
   ]
 
   print_divider
   puts "See below the different steps to follow:"
 
   steps.each_with_index do | step, index |
-    puts (index + 1).to_s + ")" + step[:description]
+    puts (index + 1).to_s + " ) " + step[:description]
   end
+
+  def generic_recipe_step
+    puts "On it!"
+    print_progress_bar
+  end
+
+  def scrape_potatoes
+    counter = 0
+    while counter < NUM_POTATOES
+      counter += 1
+      print "Scraping potato #{counter} into thick slices."
+      print_progress_bar
+    end
+  end
+
+def add_potatoes_to_the_pan
+  for counter in 1..NUM_POTATOES
+    print "Adding potato #{counter} to the pan."
+    print_progress_bar
+  end
+end
+
+def break_eggs
+  counter = 0
+  while counter < NUM_EGGS
+    counter += 1
+    print "Breaking egg #{counter}."
+    print_progress_bar
+  end
+end
